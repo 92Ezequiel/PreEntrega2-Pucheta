9a17e6f6729c999478a702bda7c11119bf4733e5
Ezequiel <jimycobain404@gmail.com>
2023-05-24 20:29:05 -0300

    pre-entrega 2

--- script.js
+++ script.js
@@ -1,36 +1,73 @@
-const general = 640
-const encargado = 690
-const administracion = 680
-let nombre = prompt ("indique su nombre");
+//se le pide al usuario un nombre y clave para generar una interaccion mas personalizada 
+let nombreUser = prompt ("Ingrese su nombre")
+let clave = parseInt(prompt("ingrese una clave numerica de 4 digitos, la misma sera guardada"))
 
-
-let semanasTrabajadas = parseInt(prompt("¿Cuantas semanas trabajaste este mes?"))
-let cantidadHorasExtras = 0
-
-for (let i = 0; i < semanasTrabajadas; i++){
-        let horasExtras = parseInt (prompt(`¿Cuantas horas extras hiciste en la semana ${i+1}?`))
-        cantidadHorasExtras += horasExtras
-    }
-    
-function evaluarHoras (horas, sueldo){
-        let horasTotal = horas * sueldo
-        let corresponde = horasTotal * 1.5 
-        alert(`${nombre}, trabajaste ${cantidadHorasExtras} horas extras este mes`)
-        alert(`te corresponden ${corresponde} pesos por tus horas extras`)
-    }
-
-let claveGeneral = 2049
-let claveEncargado = 1917
-let claveAdministracion = 1138
-let inserteClave = parseInt(prompt(`indique su clave`))
-
-if (inserteClave == claveGeneral){
-    evaluarHoras (cantidadHorasExtras, general)
-}else if (inserteClave == claveEncargado){
-    evaluarHoras (cantidadHorasExtras, encargado)
-}else if (inserteClave == claveAdministracion){
-    evaluarHoras (cantidadHorasExtras, administracion)
-}else{
-    alert ("Usted no pertenece a ninguna area o su clave es incorrecta, verifique o intente de nuevo por favor")
+if (isNaN (clave)){
+    alert("la clave debe contener solo numeros")
+    clave = parseInt(prompt("Ingrese una clave numerica de 4 digitos, la misma sera guardada"))
 }
+//objetos peliculas
+class peliculas {
+    constructor (id, titulo, director){
+        this.id = id,
+        this.titulo = titulo,
+        this.director = director
+    }
+}
+
+const pelicula1 = new peliculas(1, "Guardianes de la Galaxia Vol. 3", "James Gunn")
+const pelicula2 = new peliculas(2, "La Sirenita", "Rob Marshall")
+const pelicula3 = new peliculas(3, "Misantropo", "Damian Szifron")
+const pelicula4 = new peliculas(4, "Rapido y Furiosos 10", "Louis Leterrier")
+const pelicula5 = new peliculas(5, "Spiderman a traves del Universo", "Joaquim Dos Santos")
+const pelicula6 = new peliculas(6, "Supermario Bros", "Aaron Horvath")
+const pelicula7 = new peliculas(7, "Transformers El despertar de las bestias", "Steven Caple Jr.")
+
+const PELICULA = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5, pelicula6, pelicula7]
+//Se presentan las peliculas que estan en cartelera y se le pide al usuario que elija un id 
+alert("recuerden que las peliculas estaran disponibles para calificar hasta que sean retiradas de la cartelera")
+
+let mensaje = "Estas son las peliculas en cartelera, ingrese el numero que quiere calificar"
+
+PELICULA.forEach (e => {
+    mensaje += `\n ${e.id} - ${e.titulo} dirigida por ${e.director} \n`
+})
+
+let eleccionCinefilo = parseInt(prompt(mensaje))
+//Se individualiza la pelicula
+const peliculaACalificar = PELICULA.find (e => e.id == eleccionCinefilo)
+//funcion para validar calificacion 
+function calificar (numero, limite, alerta, devuelve){
+    while (numero > limite){
+        alert (alerta)
+        numero = parseInt(prompt(devuelve))
+    }
+    return numero;
+}
+
+eleccionCinefilo = calificar (eleccionCinefilo, 7, "no se encuentra la pelicula a calificar", mensaje)
+
+function puntaje (){
+    let reseña = parseInt(prompt("deja una breve reseña para una mejor valoracion"))
+    let recomendar = parseInt(prompt("recomienda una pelicula similar para ayudar a otros cinefilos"))
+}
+//Se crean aspectos a valorar
+let guion = parseInt(prompt(`calificar guion del 1 al 5`))
+guion = calificar (guion, 5, "calificacion no valida", "calificar guion del 1 al 5")
+let actuacion = parseInt(prompt(`calificar actuacion del 1 al 5`))
+actuacion = calificar (actuacion, 5, "calificacion no valida", "calificar actuacion del 1 al 5")
+let fotografia = parseInt(prompt(`calificar fotografia del 1 al 5`))
+fotografia = calificar (fotografia, 5, "calificacion no valida", "calificar fotografia del 1 al 5")
+
+const valoracion = [guion, actuacion, fotografia]
+const sumaValoracion = valoracion.reduce ((acum, e) => acum + e, 0)
+
+//Reduce para llegar al promedio del puntaje y obtener un puntaje final 
+alert (`la pelicula ${peliculaACalificar.titulo} recibe una calificacion de ${ Math.round(sumaValoracion / valoracion.length)}`)
+
+let valorar = puntaje ()
+
+
+alert(`Muchas gracias ${nombreUser} por tu valoracion`)
+
 
