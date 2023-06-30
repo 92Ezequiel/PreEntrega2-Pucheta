171b458474b563b7ffaf51cb2c0817422eb80f47
Ezequiel <jimycobain404@gmail.com>
2023-06-29 21:15:57 -0300

    Cambios para la entrega final

---
+++ class.js
@@ -0,0 +1,14 @@
+class Peliculas {
+    constructor (id, titulo, director, img, trama, reparto, resena, recomendacion){
+        this.id = id,
+        this.titulo = titulo,
+        this.director = director,
+        this.img = img,
+        this.trama = trama,
+        this.reparto = reparto,
+        this.puntaje = [],
+        this.puntajeFinal = "",
+        this.resena = [],
+        this.recomendacion = []
+    }
+}
--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-/*abstracion
--generar cards con las peliculas en cartelera
--el usuario elige una pelicula
--se individualiza la pelicula
--se calificican los distintos parametros
--reduce para llegar a un promedio 
--reseña y recomendacion */
 
-//se crea un class constructor para establecer objetos y pasarlos a arryas
-class Peliculas {
-    constructor (id, titulo, director, img){
-        this.id = id,
-        this.titulo = titulo,
-        this.director = director,
-        this.img = img,
-        this.puntaje = []
-    }
+//Se capturan los elementos de html para trabajar en js
+const cards = document.getElementById("cards")
+const divPuntaje = document.getElementById("puntuacion")
+
+//funcion para guardar en el LocalStorage
+function lockStorage (clave, valor){
+    localStorage.setItem(clave, valor)
 }
 
 
@@ -24,27 +15,40 @@ const pelicula3 = new Peliculas(3, "Elementos", "Peter Sohn", "https://i.blogs.e
 const pelicula4 = new Peliculas(4, "Flash", "Andy Muschietti", "https://pics.filmaffinity.com/Flash-205686824-large.jpg");
 const pelicula5 = new Peliculas(5, "Spiderman a traves del Spiderverso", "Joaquim Dos Santos", "https://m.media-amazon.com/images/M/MV5BNzQ1ODUzYjktMzRiMS00ODNiLWI4NzQtOTRiN2VlNTNmODFjXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg");
 
+//Se almacenan las peliculas en un array
 const PELICULA = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5]
 
-const CALIFICACION = []
+//Utilizo la llamada fetch para trabajar con los datos del archivo json
+fetch('./peliculas.json')
+.then((res)=> res.json())
+.then((data) => {
+    pelicula1.trama = data[0].trama
+    pelicula1.reparto = data[0].reparto
+    pelicula2.trama = data[1].trama
+    pelicula2.reparto = data[1].reparto
+    pelicula3.trama = data[2].trama
+    pelicula3.reparto = data[2].reparto
+    pelicula4.trama = data[3].trama
+    pelicula4.reparto = data[3].reparto
+    pelicula5.trama = data[4].trama
+    pelicula5.reparto = data[4].reparto
 
-function lockStorage (clave, valor){
-    localStorage.setItem(clave, valor)
-}
+})
+
 
 //funcion para crear cards a partir del array 
 function renderizarPeliculas(estrenos){
-   const cards = document.getElementById("cards")
    estrenos.forEach((pelicula => {
     let divCard = document.createElement("div")
     divCard.id = pelicula.id
     divCard.innerHTML= `
     <div class="card" style="width: 18rem; ">
-      <img src="${pelicula.img}" class="card-img-top" alt="">
+      <img src="${pelicula.img}" class="card-img-top" alt="poster de la pelicula ${pelicula.titulo}">
       <div class="card-body">
         <h5 class="card-title">${pelicula.titulo}</h5>
         <p class="card-text">Dirigida por ${pelicula.director}</p>
         <input type="button" class="btn btn-secondary" value= "calificar" onclick="calificar('${pelicula.id}')">
+        <button class="btn btn-secondary" onclick="verInformacion(${pelicula.id})"> Ver información </button>
       </div>
    </div>
     `
@@ -56,10 +60,27 @@ function renderizarPeliculas(estrenos){
 //funcion que califica las peliculas
 function calificar(idPelicula){
     let calificarPelicula = peliculasLS.find(e => e.id == idPelicula)
-    if (calificarPelicula.puntaje.length == 3){return puntajeFinal(calificarPelicula, divPuntaje)}
-
-    const divPuntaje = document.getElementById("puntuacion")
+    if (calificarPelicula.puntajeFinal > 0){
+      Swal.fire({
+        icon: "question",
+        text: "Parece que ya calificaste esta pelicula",
+        showConfirmButton: true,
+        timer: 2500
+      });
+      return;
+    }
+    cards.style.display="none";
     divPuntaje.innerHTML = `
+       <div class="cardPuntaje">
+        <div class="card" style="width: 18rem; ">
+      <img src="${calificarPelicula.img}" class="card-img-top" alt="poster de la pelicula ${calificarPelicula.titulo}">
+      <div class="card-body">
+        <h5 class="card-title">${calificarPelicula.titulo}</h5>
+        <p class="card-text">Dirigida por ${calificarPelicula.director}</p>
+        <input type="button" class="btn btn-secondary" value= "calificar" onclick="calificar('${calificarPelicula.id}')">
+        <button class="btn btn-secondary" onclick="verInformacion(${calificarPelicula.id})"> Ver información </button>
+      </div>
+   </div>
         <form id="puntajeUser"> 
             <h6> Califica los distintos aspectos de la pelicula del 1 al 10 </h6>
             <p> Guion </p>
@@ -69,8 +90,12 @@ function calificar(idPelicula){
             <p> Fotografia </p>
             <input type="number" id="puntaje3">
             <input type="submit" value="enviar">
+            <button class="btn btn-secondary" id="fichabtn" onclick="volverEstrenos()"> Ver Estrenos </button>
         </form>
+       </div>
     `
+
+
     document.getElementById("puntajeUser").addEventListener("submit", (e)=>{
         e.preventDefault()
         let infoEvent = e.target
@@ -80,50 +105,150 @@ function calificar(idPelicula){
         puntuacion= puntuacion.value
         puntuacion2 = puntuacion2.value
         puntuacion3= puntuacion3.value
+        if (puntuacion > 10 || puntuacion2 > 10 || puntuacion3 > 10) {
+          Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "La puntuación debe ser del 1 al 10",
+          showConfirmButton: true,
+          timer: 2500
+      })
+      return;
+    }else if (!puntuacion || !puntuacion2 || !puntuacion3) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Debes ingresar una puntuación para cada aspecto",
+        showConfirmButton: true,
+        timer: 2500
+      });
+      return;
+    }
+
         calificarPelicula.puntaje.push(parseInt(puntuacion),parseInt(puntuacion2), parseInt (puntuacion3))
         puntajeFinal (calificarPelicula, divPuntaje)
         lockStorage ("peliculas", JSON.stringify(peliculasLS))
     })
+
+    function puntajeFinal(peliculas, div){
+        let sumaCalificacion = peliculas.puntaje.reduce((acum, e) => acum + e, 0)
+        let promedioPuntaje = Math.round(sumaCalificacion / 3)
+        peliculas.puntajeFinal=(parseInt(promedioPuntaje))
+        div.innerHTML = `
+        <div class="cardResena">
+           <div class="card" style="width: 18rem; ">
+           <img src="${peliculas.img}" class="card-img-top" alt="poster de la pelicula ${peliculas.titulo}">
+           <div class="card-body">
+           <h5 class="card-title">${peliculas.titulo}</h5>
+           <p class="card-text">Dirigida por ${peliculas.director}</p>
+           <input type="button" class="btn btn-secondary" value= "calificar" onclick="calificar('${peliculas.id}')">
+           <button class="btn btn-secondary" onclick="verInformacion(${peliculas.id})"> Ver información </button>
+           </div>
+           </div>
+        <div class="formResena">
+           <h6 class="texto"> La pelicula ${peliculas.titulo} recibe una calificacion de ${promedioPuntaje} </h6>
+           <p class="texto"> Puedes dejar una breve reseña de la pelicula </p>
+           <form id="review">
+           <textarea class="form-control" aria-label="With textarea" id="recomendacion"></textarea>
+           <h6 class="texto"> Por ultimo, recomienda una pelicula similar a ${peliculas.titulo} a otros cinefilos </h6>
+           <input type="text" placeholder="Pelicula" id="anotherFilm">
+           <input type="submit" value="enviar reseña">
+           </form>
+        </div>
+        </div>
+           `
+           lockStorage ("puntaje", JSON.stringify(peliculas.puntajeFinal))
+           document.getElementById("review").addEventListener("submit", (e)=>{
+               e.preventDefault()
+               let infoEvent = e.target
+               let resena = infoEvent.querySelector('#recomendacion')
+               let recomendacion = infoEvent.querySelector('#anotherFilm')
+               resena = resena.value
+               recomendacion = recomendacion.value
+               peliculas.resena.push(resena)
+               peliculas.recomendacion.push(recomendacion)
+               div.innerHTML= `
+               <h5 class="texto"> ¡Muchas gracias por tu aporte! </h5>
+               <button class="btn btn-secondary" id="fichabtn" onclick="volverEstrenos()"> Ver Estrenos </button>
+               `
+               lockStorage ("resena", JSON.stringify(resena))
+               lockStorage ("recomendacion", JSON.stringify(recomendacion))
+               Swal.fire({
+               icon: "success",
+               text: "¡Calificacion Enviada!",
+               showConfirmButton: true,
+               timer: 2500
+               })
+               
+           })  
+   }
+
 }
+//Funcion para ver la ficha tencica de las peliculas
+function verInformacion (idPelicula){
+    let pelicula = peliculasLS.find((p) => p.id === idPelicula);
+        cards.style="display: none"
+        const divReview = document.getElementById("puntuacion")
+        if (pelicula.puntajeFinal === "" || pelicula.resena === "" || pelicula.recomendacion === "" ){
+        divReview.innerHTML = `
+        <div class="ficha">
+           <div class="cardFicha">
+            <div class="card" style="width: 18rem; ">
+            <img src="${pelicula.img}" class="card-img-top" alt="poster de la pelicula ${pelicula.titulo}">
+            <div class="card-body">
+            <h5 class="card-title">${pelicula.titulo}</h5>
+            <p class="card-text">Dirigida por ${pelicula.director}</p>
+            <input type="button" class="btn btn-secondary" value= "calificar" onclick="calificar('${pelicula.id}')">
+            <button class="btn btn-secondary" onclick="verInformacion(${pelicula.id})"> Ver información </button>
+          </div>
+         </div>
+         </div>
+         <div class="review">
+           <h6 class="item"> Trama: ${pelicula.trama} </h6>
+           <h6 class="item"> Reparto: ${pelicula.reparto} </h6>
+         </div>
+        </div>
+        <button class="btn btn-secondary" id="fichabtn" onclick="volverEstrenos()"> Ver Estrenos </button>
+        `}else {divReview.innerHTML = `
+        <div class="ficha">
+           <div class="cardFicha">
+            <div class="card" style="width: 18rem; ">
+            <img src="${pelicula.img}" class="card-img-top" alt="poster de la pelicula ${pelicula.titulo}">
+            <div class="card-body">
+            <h5 class="card-title">${pelicula.titulo}</h5>
+            <p class="card-text">Dirigida por ${pelicula.director}</p>
+            <input type="button" class="btn btn-secondary" value= "calificar" onclick="calificar('${pelicula.id}')">
+            <button class="btn btn-secondary" onclick="verInformacion(${pelicula.id})"> Ver información </button>
+          </div>
+         </div>
+         </div>
+         <div class="review">
+           <h6 class="item"> Trama: ${pelicula.trama} </h6>
+           <h6 class="item"> Reparto: ${pelicula.reparto} </h6>
+           <h6 class="item"> Calificacion: ${pelicula.puntajeFinal} </h6>
+           <h6 class="item"> Reseña: ${pelicula.resena} </h6>
+           <h6 class="item"> Otros usuarios recomiendan: ${pelicula.recomendacion} </h6>
 
-//a partir de un reduce y una funcion math.round obtenemos el promedio 
-function puntajeFinal(peliculas, div){
-     let sumaCalificacion = peliculas.puntaje.reduce((acum, e) => acum + e, 0)
-     let promedioPuntaje = Math.round(sumaCalificacion / 3)
-     div.innerHTML = `
-        <h6 class="texto"> La pelicula ${peliculas.titulo} recibe una calificacion de ${promedioPuntaje} </h6>
-        <p class="texto"> Puedes dejar una breve reseña de la pelicula </p>
-        <form id="review">
-        <textarea class="form-control" aria-label="With textarea" id="recomendacion"></textarea>
-        <h6 class="texto"> Por ultimo, recomienda una pelicula similar a ${peliculas.titulo} a otros cinefilos </h6>
-        <input type="text" placeholder="Pelicula" id="anotherFilm">
-        <input type="submit" value="enviar reseña">
-        </form>
+         </div>
+        </div>
+        <button class="btn btn-secondary" id="fichabtn" onclick="volverEstrenos()"> Ver Estrenos </button>
         `
-        document.getElementById("review").addEventListener("submit", (e)=>{
-            e.preventDefault()
-            let infoEvent = e.target
-            let recomendacion = infoEvent.querySelector('#recomendacion')
-            let otro = infoEvent.querySelector('#anotherFilm')
-            recomendacion = recomendacion.value
-            otro = otro.value
+        }
+    }
 
-            div.innerHTML= `
-            <h6 class="texto"> ¡Muchas gracias por tu aporte! </h6>
-            `
-            lockStorage ("reseña", JSON.stringify(recomendacion))
-            lockStorage ("recomendacion", JSON.stringify(otro))
-        })
-        
-        
-
-        
-        
+function volverEstrenos(){
+    const divReview = document.getElementById("puntuacion")
+    divReview.innerHTML= ""; 
+    cards.style.display= "flex";
 }
 
 let peliculasLS = JSON.parse(localStorage.getItem("peliculas"))
 if (!peliculasLS){
     peliculasLS = PELICULA
 }
-renderizarPeliculas(peliculasLS)
 
+//Cuando se cargue el DOM se inicia la funcion renderizarPeliculas
+document.addEventListener("DOMContentLoaded", () =>{
+    renderizarPeliculas(peliculasLS)
+
+})
